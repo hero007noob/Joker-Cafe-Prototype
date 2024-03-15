c2e94e422c3e5d52931d6790f8650e29fd456ec3
hero007coc <h9914246367@gmail.com>
2024-03-15 23:56:56 +0530

    'init'

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen h-full bg-teal-500 flex-col items-center justify-center">
+      <p>Hello Shubhi</p>
+    </main>
+  );
+}
